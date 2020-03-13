264cd848273a5b8bae0f0cad95b20b930fc54177
蔡三泽 <csz0102@gmail.com>
2020-03-13 23:09:02 +0800

    docs: modify Package.swift

--- Package.swift
+++ Package.swift
@@ -20,9 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SZAVPlayer",
-            dependencies: []),
-        .testTarget(
-            name: "SZAVPlayerTests",
-            dependencies: ["SZAVPlayer"]),
+            path: "Sources"
+        ),
     ]
 )
